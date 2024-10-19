4ec6129d1842197eed7a627409d9d69c91d12875
Michal Figiel <50012494+micfig76@users.noreply.github.com>
2024-10-19 19:11:25 +0200

    integrate togglable element

--- src/js/togglableelement.js
+++ src/js/togglableelement.js
@@ -4,4 +4,16 @@ export class TogglableElement {
   constructor(selector) {
     this.#element = document.querySelector(selector);
   }
+
+  getElement() {
+    return this.#element;
+  }
+
+  show() {
+    this.#element.classList.remove('hidden');
+  }
+
+  hide() {
+    this.#element.classList.add('hidden');
+  }
 }
